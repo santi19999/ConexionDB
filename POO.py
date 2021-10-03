43da0033f20c4e4fbb16a3e9db87f4e2e430bb3f
santi19999 <santiherrera1999@gmail.com>
2021-10-02 21:18:38 -0300

    Encapsulamos la función de cheque interno

--- POO.py
+++ POO.py
@@ -10,23 +10,49 @@ class Coche(object):
 	def arrancar(self, arrancamos):
 		self.__enmarcha=arrancamos
 
-		if self.__enmarcha:
+		if(self.__enmarcha):
+			chequeo=self.__chequeo_interno()
+
+
+		if (self.__enmarcha and chequeo):
+			print("-------------------------------------")
+			print("Chequeo Correcto, puede arrancar")
+			print("-------------------------------------"+"\n")
+
 			return "El auto está encendido"
+
+		elif(self.__enmarcha and chequeo==False):
+			return "Ocurrió algo mal en el chequeo interno, no podemos arrancar! "
+		
 		else:
 			return "El auto está apagado"
 
 	def estado(self):
 		print("El coche tiene: ", self.__ruedas," ruedas.")
-		print("Su Chasis mide: ", self.__anchoChasis,"de Ancho y ",self.__largoChasis," de largo. ")
+		print("Su Chasis mide: ", self.__anchoChasis,"de Ancho y ",self.__largoChasis," de largo. " +"\n")
 	
+	def __chequeo_interno(self):
+		print("Realizando chequeo interno")
+
+		self.gasolina="ok"
+		self.aceite="ok"
+		self.puertas="cerradas"
+
+		if(self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas"):
+			return True
+		else:
+			return False
 	def apagar(self):
 		self.__enmarcha=False
 
 
+print("-----------------Coche 1----------------------------")
 miCoche = Coche()    #Instaciar clase o crear un objeto coche
-
+print(miCoche.arrancar(True))
 miCoche.estado()
-
+print("-------------------Coche 2--------------------------")
 miCoche2 = Coche()
+print(miCoche2.arrancar(False))
+miCoche2.estado()
 
 
